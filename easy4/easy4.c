1cc184fb2feee9622887e6e1218e34bb6b9e5828
key-on423 <3531697760@qq.com>
2024-05-10 22:58:41 +0800

    hello

---
+++ easy4/easy4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+//输入一个数判断数组里是否找得到，并返还其下标
+//int main()
+//{
+//	printf("输入一个数判断数组里是否找得到，并返还其下标\n");
+//	int arr[11] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i, x;
+//	i = 0;
+//	scanf("%d", &x);
+//	arr[10] = x;
+//	while (arr[i] != x)
+//		i++;
+//	if (i < 11)
+//		printf("找到了，其在数组中下标为%d\n", i);
+//	else
+//		printf("找不到\n");
+//	return 0;
+//}
+
+//输入一个十进制数打印出他的8进制数
+//void eight(long long x);
+//int main()
+//{
+//	long long x;
+//	printf("请输入一个十进制数，程序将为你打印出他的八进制数：\n");
+//	while ((scanf("%lld", &x)) == 1)
+//	{
+//		eight(x);
+//		printf("\n请输入一个十进制数或输入q退出；程序将为你打印出他的八进制数：\n");
+//	}
+//	return 0;
+//}
+//void eight(long long x)
+//{
+//	int r=x;
+//	if(x>8)
+//	r = x % 8;
+//	if (x > 8)
+//		eight(x / 8);
+//	printf("%d", r);
+//}
+
